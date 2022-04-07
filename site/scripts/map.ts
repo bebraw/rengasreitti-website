a9f8d29e575f20f3395dfdc12f0d5cbe5e2b7c02
Juho Vepsäläinen <bebraw@gmail.com>
2022-04-07 11:27:30 +0300

    feat: Show a basic map

--- site/scripts/map.ts
+++ site/scripts/map.ts
@@ -1,1 +1,19 @@
-console.log("hello from the map");
+window.onload = () => {
+  // @ts-expect-error Figure out how to handle Leaflet typing
+  const L = window.L;
+
+  const map = L.map("map").setView([51.505, -0.09], 13);
+
+  // add the OpenStreetMap tiles
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+  }).addTo(map);
+
+  // show the scale bar on the lower left corner
+  L.control.scale({ imperial: true, metric: true }).addTo(map);
+
+  // show a marker on the map
+  L.marker({ lon: 0, lat: 0 }).bindPopup("The center of the world").addTo(map);
+};
