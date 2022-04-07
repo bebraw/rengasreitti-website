1aaa30f258fa1929f79df7302c21fb3eee43b3bb
Juho Vepsäläinen <bebraw@gmail.com>
2022-04-07 14:20:03 +0300

    feat: Show gpx

--- site/scripts/map.ts
+++ site/scripts/map.ts
@@ -10,15 +10,34 @@ window.onload = () => {
       '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
   }).addTo(map);
 
+  // Add GPX
+  const gpx = "/assets/rengasreitti.gpx";
+  new L.GPX(gpx, {
+    async: true,
+    marker_options: {
+      startIconUrl: "/assets/pin-icon-start.png",
+      endIconUrl: "/assets/pin-icon-end.png",
+      shadowUrl: "/assets/pin-shadow.png",
+      wptIconUrls: {
+        "": "/assets/pin-icon-wpt.png",
+      },
+    },
+    // @ts-expect-error TODO: Figure out how to type
+  }).on("loaded", (e) => {
+    map.fitBounds(e.target.getBounds());
+  }).addTo(map);
+
   // show the scale bar on the lower left corner
   L.control.scale({ imperial: true, metric: true }).addTo(map);
 
   // show a marker on the map
   L.marker({ lon: 0, lat: 0 }).bindPopup("The center of the world").addTo(map);
 
+  // @ts-expect-error TODO: Figure out how to type
   if (!navigator.geolocation) {
     console.log("Your browser doesn't support geolocation feature!");
   } else {
+    // @ts-expect-error TODO: Figure out how to type
     navigator.geolocation.getCurrentPosition((position) => {
       console.log("received position", position);
 
