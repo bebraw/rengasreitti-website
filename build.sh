cb1365631534554ce69824ba973cd924bd1b25d9
Juho Vepsäläinen <bebraw@gmail.com>
2022-04-08 11:34:57 +0300

    fix: Fix deno path

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.20.4
-deno run -qA https://code.velociraptor.run ci:build
+/opt/buildhome/.deno/bin/deno run -qA https://code.velociraptor.run ci:build
